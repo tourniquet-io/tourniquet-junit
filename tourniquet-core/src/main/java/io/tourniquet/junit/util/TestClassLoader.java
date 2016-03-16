f79c1f1d40626a31d503a3d84c0cd700f87037be
Gerald Mücke <gerald.muecke@devcon5.ch>
2016-03-16 19:49:00 +0100

    TQT-20 improved blacklist handling

--- tourniquet-core/src/main/java/io/tourniquet/junit/util/TestClassLoader.java
+++ tourniquet-core/src/main/java/io/tourniquet/junit/util/TestClassLoader.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,14 +16,7 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class TestClassLoader extends URLClassLoader {
 
-    /**
-     * Default set of packages to be loaded by parent class loader in order to be compatible with testing.
-     */
-    private static final Set<String> BLACKLIST;
-    static {
-        HashSet<String> set = new HashSet<>(Arrays.asList("java", "sun", "org.w3c", "org.apache.xerces"));
-        BLACKLIST = Collections.unmodifiableSet(set);
-    }
+    private final Set<String> blacklist;
 
     private final Set<String> packages;
 
@@ -37,12 +29,14 @@ public class TestClassLoader extends URLClassLoader {
      * @param packages
      *         additional packages for which this classloader should take precedence over the parent classloader
      */
-    public TestClassLoader(Collection<URL> testJars, Collection<String> packages) {
+    public TestClassLoader(Collection<URL> testJars, Collection<String> packages, String... excludePackages) {
 
         super(getURLs(testJars));
         this.packages = Collections.newSetFromMap(new ConcurrentHashMap<>());
         this.packages.addAll(packages);
-        this.packages.addAll(new JarScanner().addJar(testJars).scanPackages());
+        this.packages.addAll(new JarScanner().addJar(testJars).ignore(excludePackages).scanPackages());
+        this.blacklist = Collections.newSetFromMap(new ConcurrentHashMap<>());
+        this.blacklist.addAll(Arrays.asList(excludePackages));
     }
 
     /**
@@ -62,6 +56,7 @@ public class TestClassLoader extends URLClassLoader {
 
     /**
      * Creates a classloader for testing purposes. The classloader loads the classes from the test jars.
+     *
      * @param testJars
      *         jar files to be loaded by this classloader. For all packages and classes contained in these jars this
      *         classloader takes precedence over the parent classloader.
@@ -76,16 +71,13 @@ public class TestClassLoader extends URLClassLoader {
      */
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-
-        if (!BLACKLIST.stream().anyMatch(name::startsWith) && packages.stream().anyMatch(name::startsWith)) {
-            /*
-             * to prevent loading a class twice, we have to check, if the class is already loaded.
-             */
+        if (!blacklist.stream().anyMatch(name::startsWith) && packages.stream().anyMatch(name::startsWith)) {
+            //to prevent loading a class twice, we have to check, if the class is already loaded.
             Class<?> cls = super.findLoadedClass(name);
-            if (cls == null) {
-                cls = super.findClass(name);
+            if (cls != null) {
+                return cls;
             }
-            return cls;
+            return super.findClass(name);
         }
         /*
          * the super implementation of this method first uses the parent classloader to load a class. In case the
