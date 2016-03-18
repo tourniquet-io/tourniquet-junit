61356f30f3f54be1bdd70cc7dc7db2a1ed9ea616
Gerald Mücke <gerald.muecke@devcon5.ch>
2016-03-18 23:51:58 +0100

    (hopefully) fixed system properties test

--- tourniquet-core/src/main/java/io/tourniquet/junit/util/JUnitRunner.java
+++ tourniquet-core/src/main/java/io/tourniquet/junit/util/JUnitRunner.java
@@ -60,7 +60,8 @@ public class JUnitRunner {
      *  if the test could not be executed or result retrieved.
      */
     public static Result runClass(String className, Supplier<ClassLoader> classLoader) throws Exception {
-        final Properties currentProperties = (Properties) System.getProperties().clone();
+        final Properties origProps = new Properties();
+        origProps.putAll(System.getProperties());
         final ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
         final ClassLoader cl = classLoader.get();
         try {
@@ -71,7 +72,9 @@ public class JUnitRunner {
             return new ResultHelper().deserialize((byte[])run.invoke(runner, testClass));
         } finally {
             Thread.currentThread().setContextClassLoader(oldCl);
-            System.setProperties(currentProperties);
+            final Properties currentProps = System.getProperties();
+            currentProps.clear();
+            currentProps.putAll(origProps);
         }
     }
 
