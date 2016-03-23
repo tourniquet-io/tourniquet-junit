e29b6aec88e13f4dc95a710803e1c0a09299a775
Gerald Mücke <gerald@inkstand.io>
2016-03-23 21:16:13 +0100

    TQT-23 added support for cross-classloader selenium context transfer

--- tourniquet-pageobjects/src/main/java/io/tourniquet/pageobjects/SeleniumContext.java
+++ tourniquet-pageobjects/src/main/java/io/tourniquet/pageobjects/SeleniumContext.java
@@ -16,11 +16,20 @@
 
 package io.tourniquet.pageobjects;
 
+import static io.tourniquet.junit.util.ExecutionHelper.runUnchecked;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Proxy;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
+import io.tourniquet.junit.util.ClassStreams;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -34,26 +43,70 @@ public class SeleniumContext {
     private final Supplier<WebDriver> provider;
     private final AtomicReference<String> baseUrl = new AtomicReference<>();
 
-    public SeleniumContext(Supplier<WebDriver> provider){
+    public SeleniumContext(Supplier<WebDriver> provider) {
+
         Objects.requireNonNull(provider, "WebDriver must not be null");
         this.provider = provider;
     }
 
     /**
-     * Initializes the context of another classloader
+     * Creates a link from the other classloader to the specified context. That way it is possible to share access
+     * to the same selenium context from different classloader hierarchies.
+     *
      * @param context
+     *  the context of the current classloader hierarchy
      * @param cl
+     *  the other classloader in which the access to the given context should possible
      */
-    public static void init(SeleniumContext context, ClassLoader cl){
+    public static void init(SeleniumContext context, ClassLoader cl) {
+
+        if (!Objects.equals(context.getClass().getClassLoader(), cl) && context.getDriver().isPresent()) {
+            try {
+                final Class<?> contextClass = cl.loadClass(context.getClass().getName());
+                final Constructor constr = contextClass.getConstructor(Supplier.class);
+                final Supplier provider = () -> createProxy(context.getDriver().get(), cl);
+                final Object ctx = constr.newInstance(provider);
+                contextClass.getMethod("init").invoke(ctx);
+                contextClass.getMethod("setBaseUrl", String.class).invoke(ctx, context.getBaseUrl());
+            } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException
+                    | InvocationTargetException e) {
+                throw new RuntimeException("Could not initialize context", e);
+            }
+        }
+    }
+
+    /**
+     * Create a dynamic proxy that delegates invocation calls to from the other classloader hierarchy to the specified
+     * webdriver. That way, a webdriver can be reused in another classloader without having to serialize it.
+     *
+     * @param webDriver
+     *         the webdriver to create a proxy for
+     * @param cl
+     *         the other classloader from which access to the specified classloader should be possible
+     *
+     * @return a proxy for the webdriver
+     */
+    private static Object createProxy(final WebDriver webDriver, ClassLoader cl) {
+
+        final List<Class> interfaces = ClassStreams.selfAndSupertypes(webDriver.getClass())
+                                                   .flatMap(c -> stream(c.getInterfaces()))
+                                                   .map(c -> runUnchecked(() -> cl.loadClass(c.getName())))
+                                                   .collect(toList());
+
+        return Proxy.newProxyInstance(cl,
+                                      interfaces.toArray(new Class[interfaces.size()]),
+                                      (proxy, method, args) -> method.invoke(webDriver, args));
 
     }
 
-    public void init(){
+    public void init() {
+
         driver = Optional.of(provider.get());
         CONTEXT.set(Optional.of(this));
     }
 
-    public void destroy(){
+    public void destroy() {
+
         driver.ifPresent(WebDriver::quit);
         driver = Optional.empty();
         CONTEXT.set(Optional.empty());
@@ -61,8 +114,8 @@ public class SeleniumContext {
 
     /**
      * Global accessor to the current context which is stored in a thread local.
-     * @return
-     *  the context for the current thread.
+     *
+     * @return the context for the current thread.
      */
     public static Optional<SeleniumContext> currentContext() {
 
@@ -71,8 +124,8 @@ public class SeleniumContext {
 
     /**
      * Global accessor to the current web driver, which is stored in a thread local.
-     * @return
-     *  the current web driver
+     *
+     * @return the current web driver
      */
     public static Optional<WebDriver> currentDriver() {
 
@@ -81,8 +134,8 @@ public class SeleniumContext {
 
     /**
      * The web driver of the context
-     * @return
-     *  a Selenium WebDriver
+     *
+     * @return a Selenium WebDriver
      */
     public Optional<WebDriver> getDriver() {
 
@@ -91,8 +144,8 @@ public class SeleniumContext {
 
     /**
      * The base URL for the current context used to resolve relative URLs.
-     * @return
-     *  the current BaseUrl
+     *
+     * @return the current BaseUrl
      */
     public String getBaseUrl() {
 
@@ -101,10 +154,12 @@ public class SeleniumContext {
 
     /**
      * Sets the base URL for the current context used to resolve relative URLs.
+     *
      * @param baseUrl
-     *  the new base URL
+     *         the new base URL
      */
-    public void setBaseUrl(String baseUrl){
+    public void setBaseUrl(String baseUrl) {
+
         Objects.requireNonNull(baseUrl, "BaseUrl must not be empty");
         this.baseUrl.set(baseUrl);
     }
