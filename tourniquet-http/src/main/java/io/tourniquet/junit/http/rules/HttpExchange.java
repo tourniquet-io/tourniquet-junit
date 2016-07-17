f4187cf05066dadb262a671522bcbebd84db2fbd
Gerald Mücke <gerald.muecke@devcon5.ch>
2016-07-18 01:14:02 +0200

    TQT-40 fixed sonar issues

--- tourniquet-http/src/main/java/io/tourniquet/junit/http/rules/HttpExchange.java
+++ tourniquet-http/src/main/java/io/tourniquet/junit/http/rules/HttpExchange.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015-2016 DevCon5 GmbH, info@devcon5.ch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.tourniquet.junit.http.rules;
 
 import static java.util.stream.Collectors.toCollection;
@@ -62,7 +78,9 @@ public class HttpExchange {
                 throw new RuntimeException(e);
             }
         }
-        return this.payload;
+        final byte[] data = new byte[this.payload.length];
+        System.arraycopy(this.payload, 0, data, 0, this.payload.length);
+        return data;
     }
 
     public OutputStream getOutputStream() {
